4e3bd41b068765c501ab88e34fdf69ff5ac96054
Shai Taharlev <stagepi@bama-box.com>
2025-09-05 17:07:19 +0300

    Added license header
    Signed-off-by: Shai Taharlev <stagepi@bama-box.com>

--- stagepi-package/usr/local/stagepi/ui/core/network_manager.py
+++ stagepi-package/usr/local/stagepi/ui/core/network_manager.py
@@ -1,4 +1,22 @@
+
 # core/network_manager.py
+"""
+Stage Pi: Open source stagebox firmware
+
+Copyright (C) 2025 Bama Box ltd.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import subprocess
 import logging
 
